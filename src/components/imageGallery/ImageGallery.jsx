fd880290d8f38d3ad4855edb29faa7bcddc6be45
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-04 18:22:21 +0300

    Updated on first page after new query

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -8,6 +8,7 @@ import { ImageGalleryUl, Container } from './ImageGallery.styled';
 import { fetchGalleryImg } from '../../Api/fetchGalleryImg';
 
 export function ImageGallery({ showModal, searchQuery }) {
+  const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
@@ -25,10 +26,12 @@ export function ImageGallery({ showModal, searchQuery }) {
     if (searchQuery === '') {
       return;
     }
-
-    // setImages(null);
-    // setPage(1);
-    // setHiddenBnt(true);
+    if (searchQuery !== query) {
+      setQuery(searchQuery);
+      setImages([]);
+      setPage(1);
+      setHiddenBnt(true);
+    }
 
     getImages(searchQuery, page);
   }, [searchQuery, page]);
@@ -40,13 +43,14 @@ export function ImageGallery({ showModal, searchQuery }) {
         if (hits.length === 0) {
           showErrorMsg();
           setHiddenBnt(true);
+          return;
         } else {
           setImages(prevImages => [...prevImages, ...hits]);
           setHiddenBnt(false);
-        }
-        if (12 * page > totalHits) {
-          showErrorMsg();
-          setHiddenBnt(true);
+          if (12 * page > totalHits) {
+            showErrorMsg();
+            setHiddenBnt(true);
+          }
         }
       })
       .catch(error => error)
